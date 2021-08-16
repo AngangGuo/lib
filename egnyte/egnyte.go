f64e2d489e9303a81042f9a99e2ea81bd8998789
AMK <amkguo@gmail.com>
2021-08-16 16:38:05 -0700

    create Egnyte struct

--- egnyte/egnyte.go
+++ egnyte/egnyte.go
@@ -2,9 +2,19 @@ package egnyte
 
 import (
 	"fmt"
-	"strings"
+	"io"
+	"net/http"
+	"os"
 	"time"
 )
+
+// Egnyte
+type Egnyte struct {
+	Token             string
+	facility          string
+	allFieldsFileLink string
+	linkDate          string
+}
 
 // facility names
 const (
@@ -12,9 +22,9 @@ const (
 	TorontoFacility   = "Toronto"
 )
 
-// RL Inventory All Fields file download link
-// Example link for Vancouver: /Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Vancouver, BC (RL).csv
-// Example link for Toronto: /Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Toronto, ON.csv
+// Example for RL Inventory All Fields files
+// [/Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Vancouver, BC (RL).csv]
+// [/Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Toronto, ON.csv]
 const (
 	linkPrefix = "https://cloudblue.egnyte.com/pubapi/v1/fs-content/Shared/Operations-RL/Daily%20RL%20All%20Fields%20Reports/RL%20Inventory%20All%20Fields_"
 	// ddmmyyyy format used in the link
@@ -23,37 +33,75 @@ const (
 	torontoSuffix   = "_Toronto%2C%20ON.csv"
 )
 
-// getFileLink return the direct api link of the latest RL Inventory All Fields file of the facility
-func getFileLink(facility string) (string, error) {
-	ddmmyyyyDate := time.Now().AddDate(0, 0, -1).Format(linkDateFormat)
-
-	var link string
-	switch facility {
-	case VancouverFacility:
-		// For Vancouver: /Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Vancouver, BC (RL).csv
-		link = linkPrefix + ddmmyyyyDate + vancouverSuffix
-		return link, nil
-	case TorontoFacility:
-		// /Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Toronto, ON.csv
-		link = linkPrefix + ddmmyyyyDate + torontoSuffix
-		return link, nil
-	default:
-		link = ""
-		return link, fmt.Errorf("getFileLink: facility %s is not supported", facility)
+// New create a new Egnyte instance
+// set EgnyteToken="69y...ctp
+// egnyteTokenName := "EgnyteToken"
+// token := os.Getenv(egnyteTokenName)
+// if token == "" {
+// 	return fmt.Errorf("can't find the egnyte token from environment variable: %s", egnyteTokenName)
+// }
+// egnyte := Egnyte.New(token)
+func New(token string) *Egnyte {
+	return &Egnyte{
+		Token: token,
+		// always download the latest file
+		// The latest uploaded file on website is from yesterday
+		linkDate: time.Now().AddDate(0, 0, -1).Format(linkDateFormat),
 	}
 }
 
-// GetSavedFileName returns the .csv file name according to facility names
-// facility can only be Vancouver or Toronto.
-// the file name is used to save the Daily RL All Fields Reports
-// Extract this function to coordinate the download program and the report program
-func GetSavedFileName(facility string) (string, error) {
-	facility = strings.Title(strings.TrimSpace(facility))
-
+func (e *Egnyte) SetFacility(facility string) error {
 	switch facility {
 	case VancouverFacility, TorontoFacility:
-		return fmt.Sprintf("%s.csv", facility), nil
+		e.facility = facility
+		e.setInventoryAllFieldsFileLink()
+		return nil
 	default:
-		return "", fmt.Errorf("GetSavedFileName: wrong facility %s", facility)
+		return fmt.Errorf("setFacility: facility %s is not supported", facility)
 	}
+}
+
+// getInventoryAllFieldsFileLink return the direct api link of the latest RL Inventory All Fields file of the facility
+func (e *Egnyte) setInventoryAllFieldsFileLink() {
+	switch e.facility {
+	case VancouverFacility:
+		// For Vancouver: /Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Vancouver, BC (RL).csv
+		e.allFieldsFileLink = linkPrefix + e.linkDate + vancouverSuffix
+	case TorontoFacility:
+		// /Shared/Operations-RL/Daily RL All Fields Reports/RL Inventory All Fields_13072021_Toronto, ON.csv
+		e.allFieldsFileLink = linkPrefix + e.linkDate + torontoSuffix
+		//default:
+		//	e.allFieldsFileLink = ""
+		//	return fmt.Errorf("setInventoryAllFieldsFileLink: facility %s is not supported", facility)
+	}
+}
+
+// Download will download the Inventory All Fields file(.csv) from Egnyte and write the file to filePath
+func (e *Egnyte) Download(filePath string) (int64, error) {
+	// number of bytes downloaded
+	var n int64
+
+	req, err := http.NewRequest("GET", e.allFieldsFileLink, nil)
+	if err != nil {
+		return n, err
+	}
+	req.Header.Add("Authorization", "Bearer "+e.Token)
+	client := &http.Client{}
+	response, err := client.Do(req)
+	if err != nil {
+		return n, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != 200 {
+		return n, fmt.Errorf("wrong status code: %v", response.StatusCode)
+	}
+
+	out, err := os.Create(filePath)
+	if err != nil {
+		return n, err
+	}
+	defer out.Close()
+
+	return io.Copy(out, response.Body)
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/AngangGuo/lib
 
 go 1.16
 
-require (
-	github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0
-	golang.org/x/tools v0.1.5 // indirect
-)
+require github.com/360EntSecGroup-Skylar/excelize/v2 v2.4.0
